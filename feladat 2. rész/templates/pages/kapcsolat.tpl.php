25248e508eddb3f0766c10c083c32ca306b2c80c
dtricsi <95308923+dtricsi@users.noreply.github.com>
2021-11-30 19:20:15 +0100

    feladat 2. rész

---
+++ feladat 2. rész/templates/pages/kapcsolat.tpl.php
@@ -0,0 +1,4 @@
+<h2>Adatok:</h2>
+<p>Ügyvezető: <strong>Takács Kati</strong></p>
+<p>E-mail: <strong>allatesember@gmail.com</strong></p>
+<a href="http://localhost/feladat/?oldal=emailkuldes">Üzenet küldése</a>
