795a8b113b50d33161a0b7cd38d2c9c50983cffd
dtricsi <95308923+dtricsi@users.noreply.github.com>
2021-12-06 22:21:21 +0100

    Delete feladat directory

--- feladat/templates/pages/config.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $MAPPA = './kepek/';
-    $TIPUSOK = array ('.jpg', '.png');
-    $MEDIATIPUSOK = array('image/jpeg', 'image/png');
-    $DATUMFORMA = "Y.m.d. H:i";
-    $MAXMERET = 500*1024;
-?>
