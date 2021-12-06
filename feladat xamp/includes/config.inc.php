fb0c67cd445b579a1631146823b26df0c85684ee
dtricsi <95308923+dtricsi@users.noreply.github.com>
2021-12-06 22:37:54 +0100

    xamp feladat

---
+++ feladat xamp/includes/config.inc.php
@@ -0,0 +1,35 @@
+<?php
+$ablakcim = array(
+    'cim' => 'Állat és Ember Egyesület',
+);
+
+$fejlec = array(
+    'kepforras' => 'logojelveny.png',
+    'kepalt' => 'logo',
+	'cim' => 'Állat és Ember Egyesület',
+	'kepforras2' => 'kutyaesmacska.jpg',
+	'kepforras3' => 'kutyaesmacska2.jpg',
+	'motto' => ''
+);
+
+$lablec = array(
+    'copyright' => 'Copyright '.date("Y").'.',
+    'ceg' => 'Állat és Ember Egyesület'
+);
+
+$oldalak = array(
+	'/' => array('fajl' => 'bemutatkozas', 'szoveg' => 'Bemutatkozás', 'menun' => array(1,1)),
+	'tamogatoink' => array('fajl' => 'tamogatoink', 'szoveg' => 'Támogatóink', 'menun' => array(1,1)),
+	'galeria' => array('fajl' => 'galeria', 'szoveg' => 'Galéria', 'menun' => array(1,1)),
+	'kepfeltoltes' => array('fajl' => 'feltolt', 'szoveg' => 'Képfeltöltés', 'menun' => array(0,1)),
+	'kapcsolat' => array('fajl' => 'kapcsolat', 'szoveg' => 'Kapcsolat', 'menun' => array(1,1)),	
+	'emailkuldes' => array('fajl' => 'emailkuldes', 'szoveg' => 'Email küldése', 'menun' => array(0,0)),
+	'tablazat' => array('fajl' => 'tablazat', 'szoveg' => 'Táblázat', 'menun' => array(1,1)),
+    'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
+    'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
+    'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
+    'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
+);
+
+$hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+?>
