3f2f4c53feabb5b5e8ab5bdaec65e116acd0dace
dtricsi <95308923+dtricsi@users.noreply.github.com>
2021-12-07 18:10:07 +0100

    teljes feladat

---
+++ feladat/templates/pages/kapcsolat.tpl.php
@@ -0,0 +1,6 @@
+<h2>Adatok:</h2>
+<p>Ügyvezető: <strong>Takács Kata</strong></p>
+<p>E-mail: <strong>allatesember@gmail.com</strong></p>
+<p>Adószám: 18195376-2-42  Számlaszám: 12010453-01006150-00100004   Iban: HU68 1201 0453 0100 6150 0010 0004   Swift: UBRTHUHB
+Állat és Ember Á.V.E. Yorkie fajtamentés számlaszám: 12010453-01006150-00200001   Iban: HU47 1201 0453 0100 6150 0020 0001   Swift: UBRTHUHB</p>
+<a href="./templates/pages/emailkuldes.tpl.php">Üzenet küldése</a>
---
+++ feladat/templates/pages/tamogatoink.tpl.php
@@ -0,0 +1,17 @@
+<h2>Támogatóink</h2>
+<p>Szeretnénk itt is megköszönni azoknak a cégeknek, magánszemélyeknek a segítségét, akik egyesületünk munkáját segítik pénz- vagy tárgyi adománnyal, társadalmi munkával, akciókkal, kedvezménnyel. Valamennyiüket itt nem soroljuk fel, és van olyan is, aki névtelen szeretne maradni. Támogatóink sokfélék: találsz köztük egyszemélyes vagy családi vállalkozást, de multinacionális céget is; olyat, ahol a munkáltató igénye volt a segítségnyújtás, de olyat is, ahol a dolgozói kollektíva szervezte meg és nyújtja, nem esetenként csak, hanem kinyilvánított célként: folyamatosan.
+Formabontás gyanánt támogatóinkat kértük meg, kedvcsinálónak másoknak, hogy írjanak magukról, és arról, milyen indíttatásból, miben, hogyan segítenek.
+</p>
+
+<img src="./images/fortkft.jpg" width="100" height="100">
+<h3>Fort Kft.</h3>
+<p>Az anyagi támogatás mellett hozunk-viszünk kutyusokat állatorvoshoz, ideiglenes gazdihoz, végleges gazdihoz, és az egyesület szinte valamennyi rendezvényén részt veszünk, mint szervezők.
+Ebben segítenek családunk tagjai - apukám, lányom, Manka -, de a cégben dolgozó kollegák is, akik önként szívesen jönnek a cél érdekében.</p>
+<p>„Véletlenül” egy internetes keresés kapcsán- szigorúan munkaügy – rátaláltam egy kutyusra, akinek szomorú sorsa nagyon megindított. Jó-jó, de hol van ebben a társadalmi szerepvállalás ?! A kutyus az Állat és Ember Állatvédő Egyesületnél lakott – ezt csak így tudom definiálni, hiszen panzióban lakott – Többszöri látogatás után örökbe fogadtuk Efit, aki 2 éve családunk és irodánk új tagja.
+Eleinte csak ennyi kapcsolatunk volt az egyesülettel, aztán szép fokozatosan lelkes tagjai lettünk.</p>
+<img src="./images/salus.jpg" width="100" height="100">
+<h3>Salus</h3>
+<p>Cégünkben a Salus Kft-ben sokunknak van, volt vagy lesz háziállata. Fontosnak gondoltuk, hogy lehetőségeinkhez mérten segítséget nyújtsunk egy olyan szervezetnek, mely ugyanúgy elengedhetetlennek tartja a kedvencek megfelelő gondozását, mint mi magunk.
+Az egyesületet termékeinkkel segítjük.
+A SALUS Kft. a gyógyászati segédeszköz gyártása és forgalmazása terén jelentős múltra visszatekintő, több, mint húsz éve működő vállalkozás.</p>
+<a href="http://www.xn--llatsember-r4a4h.hu/hirek.php?pr_id=23">aloldal</a>
